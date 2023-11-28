5199cd73f1e94465daac13ea776df12ce105a5cb
Marek <marek@manyz.cz>
2023-11-28 21:41:35 +0100

    Init commit

---
+++ remotely-addon/DockerMain.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Entered main script."
+
+ln -s /data /remotely-data
+
+RemotelyData=/remotely-data
+
+AppSettingsVolume=/remotely-data/appsettings.json
+AppSettingsSrc=/app/appsettings.json
+
+if [ ! -f "$AppSettingsVolume" ]; then
+	echo "Copying appsettings.json to volume."
+	cp "$AppSettingsSrc" "$AppSettingsVolume"
+fi
+
+if [ -f "$AppSettingsSrc" ]; then
+	rm "$AppSettingsSrc"
+fi
+
+ln -s "$AppSettingsVolume" "$AppSettingsSrc"
+
+echo "Starting Remotely server."
+exec /usr/bin/dotnet /app/Remotely_Server.dll
